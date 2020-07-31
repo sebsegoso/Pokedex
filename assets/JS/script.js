1dda1bccf49a038f94c98258c3a2f291b55d1162
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2020-07-31 02:59:41 -0400

    add Pokemon's Type

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,7 +9,7 @@ const buscarPokemon = () => {
     let busqueda = $("#Buscador").val();
     let urlAPI = `https://pokeapi.co/api/v2/pokemon/${busqueda}`;
 
-
+    //importar ajax
     $.ajax({
         url: urlAPI,
         success: result => {
@@ -17,7 +17,7 @@ const buscarPokemon = () => {
             pokemon = {
                 nombre: result.name,
                 id: result.id,
-                //Tipo = result.types.type.name ,
+                tipo : result.types[0].type.name,
                 ataque: result.stats[1].base_stat,
                 defensa: result.stats[2].base_stat,
                 hp: result.stats[0].base_stat,
@@ -28,7 +28,69 @@ const buscarPokemon = () => {
                 imgBack: result.sprites.back_default
             };
 
+            //Traduciendo el tipo de pokemon
+            if (pokemon.tipo == "electric") {
+                pokemon.tipo = "Eléctrico"
+            } 
+            else if (pokemon.tipo == "normal") {
+                pokemon.tipo = "Normal"
+            }
+            else if (pokemon.tipo == "fighting") {
+                pokemon.tipo = "Luchador"
+            }
+            else if (pokemon.tipo == "flying") {
+                pokemon.tipo = "Volador"
+            }
+            else if (pokemon.tipo == "poison") {
+                pokemon.tipo = "Venenoso"
+            }
+            else if (pokemon.tipo == "rock") {
+                pokemon.tipo = "Roca"
+            }
+            else if (pokemon.tipo == "bug") {
+                pokemon.tipo = "Bicho"
+            }
+            else if (pokemon.tipo == "ghost") {
+                pokemon.tipo = "Fantasma"
+            }
+            else if (pokemon.tipo == "steel") {
+                pokemon.tipo = "Acero"
+            }
+            else if (pokemon.tipo == "fire") {
+                pokemon.tipo = "Fuego"
+            }
+            else if (pokemon.tipo == "water") {
+                pokemon.tipo = "Agua"
+            }
+            else if (pokemon.tipo == "psychic") {
+                pokemon.tipo = "Psíquico"
+            }
+            else if (pokemon.tipo == "ice") {
+                pokemon.tipo = "Hielo"
+            }
+            else if (pokemon.tipo == "dragon") {
+                pokemon.tipo = "Dragón"
+            }
+            else if (pokemon.tipo == "dark") {
+                pokemon.tipo = "Oscuro"
+            }
+            else if (pokemon.tipo == "fairy") {
+                pokemon.tipo = "Hada"
+            }
+            else if (pokemon.tipo == "unknown") {
+                pokemon.tipo = "Desconocido"
+            }
+            else if (pokemon.tipo == "grass") {
+                pokemon.tipo = "Planta"
+            }
+            else if (pokemon.tipo == "shadow") {
+                pokemon.tipo = "Sombra"
+            };
+            
+            
+            $("#Info").empty(); //Para vaciar el div contenedor y mostrar sólo 1 pokemon en pantalla
 
+            //Agregando info al documento
             $("#Info").prepend(`
             <div class="row">
             <h2 class="nombre col-12">${pokemon.nombre.toUpperCase()} #${pokemon.id}</h2>
@@ -39,6 +101,7 @@ const buscarPokemon = () => {
                             <img src=${pokemon.imgBack} alt="" />
                         </div>
                         <p>Id pokedex: #${pokemon.id}</p>
+                        <p>Tipo: ${pokemon.tipo}</p>
                     </div>
                 </div>
                 <div class="info-wrapper col-12 col-lg-3">
@@ -58,7 +121,7 @@ const buscarPokemon = () => {
                     </div>
                 </div>
                 </div>
-                <hr />`);
+                <hr />`).fadeIn(3000);
 
             var chart = new CanvasJS.Chart("chartContainer", {
                 animationEnabled: true,
@@ -67,7 +130,7 @@ const buscarPokemon = () => {
                     startAngle: 60,
                     innerRadius: 80,
                     indexLabelFontSize: 17,
-                    indexLabel: "{label} - {y}",
+                    indexLabel: "{label}: {y}",
                     toolTipContent: "<b>{label}:</b> {y}",
                     dataPoints: [
                         { y: pokemon.ataque, label: "Ataque" },
@@ -80,6 +143,8 @@ const buscarPokemon = () => {
                 }]
             });
             chart.render();
+
+        busqueda = "";
         }
     });
 }
